98810d4d292617af2eda856e867e5cd1d1753b40
brenu <bvsousa64@gmail.com>
2020-06-28 01:59:17 -0300

    :construction: Implement requests base

--- direxceed.py
+++ direxceed.py
@@ -1,1 +1,6 @@
-print('Hello World!');
+import requests
+request = requests.get('http://www.google.com')
+if request.status_code == 200:
+    print('Web site exists')
+else:
+    print('Web site does not exist') 
