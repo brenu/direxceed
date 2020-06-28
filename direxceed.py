3bd6f8003ed73d4e83c3a1831bc15bc2a6dbfdc2
brenu <bvsousa64@gmail.com>
2020-06-28 02:46:39 -0300

    :rocket: Implement file reading and file URLs verifying

--- direxceed.py
+++ direxceed.py
@@ -1,6 +1,22 @@
 import requests
-request = requests.get('http://www.google.com')
-if request.status_code == 200:
-    print('Web site exists')
-else:
-    print('Web site does not exist') 
+
+# Initializing variables
+counter = 0
+
+# Opening file
+f = open('test.txt', 'r')
+
+# Looking for available pages inside the wordlist
+for i in f:
+    t = i
+    i = t[:-1]
+    url = 'https://{}'.format(i)
+    try:
+        request = requests.get(url)
+        if request.status_code == 200:
+            print('Web site {} exists'.format(url))
+            counter = counter + 1
+    except:
+        counter = counter
+
+print('The wordlist has {} valid links'.format(counter))
