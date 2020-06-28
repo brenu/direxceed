7f7a2d59f65d72b7d7f5aeb0af631675b769a763
brenu <bvsousa64@gmail.com>
2020-06-28 14:55:38 -0300

    :construction: Make DirExceed look for directories inside example website

--- direxceed.py
+++ direxceed.py
@@ -10,13 +10,13 @@ f = open('test.txt', 'r')
 for i in f:
     t = i
     i = t[:-1]
-    url = 'https://{}'.format(i)
+    url = 'https://www.google.com{}'.format(i)
     try:
         request = requests.get(url)
         if request.status_code == 200:
-            print('Web site {} exists'.format(url))
+            print('Directory {} exists'.format(url))
             counter = counter + 1
     except:
         counter = counter
 
-print('The wordlist has {} valid links'.format(counter))
+print('The wordlist has {} valid directories'.format(counter))
