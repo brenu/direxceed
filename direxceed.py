4385428d0f7ce0b9306bb4667b0968416ecb7293
brenu <bvsousa64@gmail.com>
2020-06-29 07:25:14 -0300

    :rocket: Show banner explaining how to use the program

--- direxceed.py
+++ direxceed.py
@@ -8,16 +8,23 @@ counter = 0
 f = open('test.txt', 'r')
 
 # Looking for available pages inside the wordlist
-for i in f:
-    t = i
-    i = t[:-1]
-    url = 'https://{}{}'.format(sys.argv[1],i)
-    try:
-        request = requests.get(url)
-        if request.status_code == 200:
-            print('Directory {} exists'.format(url))
-            counter = counter + 1
-    except:
-        counter = counter
-
-print('The wordlist has {} valid directories'.format(counter))
+if len(sys.argv) - 1 > 1:
+    for i in f:
+        t = i
+        i = t[:-1]
+        url = 'https://{}{}'.format(sys.argv[1],i)
+        try:
+            request = requests.get(url)
+            if request.status_code == 200:
+                print('Directory {} exists'.format(url))
+                counter = counter + 1
+        except:
+            counter = counter
+    print('The wordlist has {} valid directories'.format(counter))
+else: 
+    print()
+    print('                        DirExceed                        ')
+    print('                 Created by brenu (Exceed)               ')
+    print()
+    print('                         Usage                           ')
+    print('--------- python3 direxceed.py www.example.com ----------')
